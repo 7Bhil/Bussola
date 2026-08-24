415509af9147f5f4dd33bb8a19eab42581a00844
7Bhil <7bhilal.chitou7@gmail.com>
2026-08-24 14:15:15 +0100

    feat(gallery): connect projects and actions dynamically to MongoDB

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-caf3a6a6'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.kke6tbdahug"
+    "revision": "0.p0b6n4cukc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
